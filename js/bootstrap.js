97b85ecf6f3607621827fd84e5a25630b7b834c5
Gerardo Alderete <gerardoalderete92@gmail.com>
2019-01-03 22:47:14 -0600

    DETALLES FINALES Y MEJORAS DEL PROYECTO

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -1,8 +1,14 @@
 ;
 ((d) => {
-    d.addEventListener('DOMContentLoaded', e => {
+d.addEventListener('DOMContentLoaded', e => {
         const copy = d.querySelector('.Footer small')
 
         copy.innerHTML = `&copy; ${new Date().getFullYear()} @jonmircha`
-    })
+        $('.navbar-nav>li>a').on('click', () => $('.navbar-collapse').collapse('hide'))
+
+        const scroll = new SmoothScroll('a[href*="#"]', {
+            offset: 50,
+            selectorHeader: '.navbar'
+        })
+})
 })(document);
